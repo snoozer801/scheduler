b1564f4a2e1051181567cf1d537a9028645382ff
David <monst3rm3tal@gmail.com>
2018-05-05 17:16:15 -0400

    Scheduling App

---
+++ lib/scheduler_web/router.ex
@@ -0,0 +1,27 @@
+defmodule SchedulerWeb.Router do
+  use SchedulerWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", SchedulerWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/users", UserController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", SchedulerWeb do
+  #   pipe_through :api
+  # end
+end
